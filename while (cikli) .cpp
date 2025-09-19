8c39f34836de33bf56dfa436bcb7a8b6b0ac92a6
Ivars67 <ivars.kristapsons2008@gmail.com>
2025-09-19 08:42:54 +0300

    Add files via upload

---
+++ while (cikli) .cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int i = 1;
+    
+//var arī i !=10
+    while(i<=10){
+        
+        cout<<i<<endl;
+        
+        i = i+1;
+//var arī i++ vai i+=1
+
+    }
+    
+    
+    
+}
